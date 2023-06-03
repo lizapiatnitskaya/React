2d083b558e4a33d59b973567a3c8b858fa7f43f5
lizapiatnitskaya <piatnitskayaliza@gmail.com>
2023-06-03 21:25:04 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Star } from './components/Star';
+import { Header } from './components/Header';
+import { Navbar } from './components/Navbar';
+import { Profile } from './components/Profile';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-     <Star title={"Hello,world!"} collapsed={false}/>
-     <Star title={"Hello!"} collapsed={true}/>
-    </div> 
+    <div className="app-wrapper">
+       <Header/>
+       <Navbar/>
+       <Profile/>
+    </div>
   );
 }
 
---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const Profile=()=>{
+    return(
+        <div className="content">
+         <div>
+           <img src=""/>
+         </div>
+         <div>
+           ava+discription
+         </div>
+         <div>
+           My posts
+         </div>
+         <div>
+           New post
+         </div>
+         <div>
+           post 1
+         </div>
+       </div> 
+       
+    )
+}
